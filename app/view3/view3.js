eeb8caa5173a30a4f377d28e9999a8e432881b65
maddy <mdeason89@gmail.com>
2015-11-27 15:28:58 -0500

    adds view3

--- app/view3/view3.js
+++ app/view3/view3.js
@@ -1,14 +1,15 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+angular.module('myApp.view3', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
+  $routeProvider.when('/view3', {
+    templateUrl: 'view3/view3.html',
+    controller: 'View3Ctrl'
   });
 }])
 
-.controller('View2Ctrl', [function() {
+.controller('View3Ctrl', [function() {
 	
+	]
 }]);
